b406e2c98b98ac45c3a1212aca93bf1fecea1d96
Tarun Gupta <61519059+tarunguptaraja@users.noreply.github.com>
2020-08-01 10:17:50 +0530

    Update

---
+++ app/build/generated/source/buildConfig/release/mooc/vandy/java4android/calculator/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package mooc.vandy.java4android.calculator;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "mooc.vandy.java4android.calculator";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
